4e8117e3997436f0fb88dc51d655a9c66e83b6b4
lyskz <lyski.zach@icloud.com>
2023-06-15 15:24:36 -0700

    navigation js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,11 +2,55 @@
 import React from 'react';
 
 const Navigation = ({ currentPage, handlePageChange }) => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <ul className="navigation">
+      <li className="nav-item">
+        <a
+          href="#Home"
+          onClick={() => handlePageChange('Home')}
+          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+          >
+          Home
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#AboutMe"
+          onClick={() => handlePageChange('AboutMe')}
+          className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}
+        >
+          About Me
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#Portfolio"
+          onClick={() => handlePageChange('Portfolio')}
+          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+        >
+          Portfolio
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#Contact"
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#Resume"
+          onClick={() => handlePageChange('Resume')}
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          Resume
+        </a>
+      </li>
+    </ul>
+  );
 };
 
 export default Navigation;
