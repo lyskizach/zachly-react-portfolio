9658f052e1e79ba2eef9c37d187445f9dd8d6f8a
lyskz <lyski.zach@icloud.com>
2023-06-15 16:10:22 -0700

    added footer links and icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,33 @@ const Footer = () => {
     return (
         <div className='footer'>
             <ul className='footer-content'>
-                <li>Github</li>
-                <li>LinkedIn</li>
-                <li>Leetcode</li>
+                <li>Github
+                    <a href='https://github.com/lyskizach' target='_blank' rel='noreferrer'>
+                        <img 
+                        className='img-icon' 
+                        src='./images/GithubIcon.jpg' 
+                        alt='Github Icon'>
+                        </img>
+                    </a>
+                </li>
+                <li>LinkedIn
+                    <a href='https://www.linkedin.com/' target='_blank' rel='noreferrer'>
+                        <img
+                        className='img-icon'
+                        src='./images/LinkedinIcon.jpg'
+                        alt='Linkedin Icon'>
+                        </img>
+                    </a>
+                </li>
+                <li>Leetcode
+                    <a href='https://leetcode.com/lyskz/' target='_blank' rel='noreferrer'>
+                        <img
+                        className='img-icon'
+                        src='./images/LeetcodeIcon.jpg'
+                        alt='Leetcode Icon'>
+                        </img>
+                    </a>
+                </li>
             </ul>
         </div>
     );
