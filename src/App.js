0e6abb6902399c06eb55ae448175a832b76b4720
lyskz <lyski.zach@icloud.com>
2023-06-15 13:04:49 -0700

    more starting files and images

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+// import components
+
+function App() {
+    return (
+        <div className='react-app'>
+            {/* render imported particles */}
+        </div>
+    );
+};
+
+export default App;
