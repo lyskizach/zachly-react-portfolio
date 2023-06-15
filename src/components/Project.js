12d6a88907f57d98e9d12305b31aa61a50c7edd9
lyskz <lyski.zach@icloud.com>
2023-06-15 15:55:23 -0700

    basic project js outline

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,3 +9,28 @@
 // A link to the corresponding GitHub repository
 
 // A single Project component that will be used multiple times in the Portfolio section
+import React from 'react';
+
+const Project = () => {
+    return (
+        <div className='project'>
+            <ul className='project-list'>
+                <li>BookClub Deployed Link then Repo Link
+                    <a href='https://b-crites.github.io/BookClub/' target='_blank' rel='noreferrer'>
+
+                    </a>
+                    <a href='https://github.com/b-crites/BookClub' target='_blank' rel='noreferrer'>
+                        
+                    </a>
+                </li>
+                <li>Cook's Companion Repo Link
+                    <a href='https://github.com/andrewmuhn/cooks-companion' target='_blank' rel='noreferrer'>
+
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Project;
